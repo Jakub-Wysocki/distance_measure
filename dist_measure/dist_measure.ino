e53c9732bb29c397bd86eeb680cbd95a6ae97836
Jakub-Wysocki <jakubwysocki@protonmail.com>
2020-09-08 06:17:54 +0200

    Changes in ISR for input capture mode

--- dist_measure/dist_measure.ino
+++ dist_measure/dist_measure.ino
@@ -1,6 +1,7 @@
 #include <util/delay.h>
 
-unsigned short t_start = 0, t_stop = 0, temp = 0;
+unsigned short t_start = 0, t_stop = 0;
+float temp = 0;
 
 void SetupTimer1() {
   noInterrupts();
@@ -9,57 +10,72 @@ void SetupTimer1() {
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
+  
+  ICR1 = 0 // set input caputre register to 0
 
   OCR1A = 46400; //Count up to 46400. 1 cycle is 0.5uS. Should I add additional clock cycles? Noice canceler is turned off. 
   TCCR1B |= (1 << WGM12); // Set CTC mode
   TCCR1B |= (1 << CS11);  // Set prescaler to 8
-  TCCR1B |= (1 << ICES1);  // Set Input Capture Mode on rising edge
+  TCCR1B |= (1 << ICES1) | (1 << ICNC1);  // Set Input Capture Mode on rising edge and the Noise Canceler
   TIMSK1 |= (1 << OCIE1A) | (1 << ICIE1); // Output Compare Match A Interrupt Enable, and Input capture interrupt
   interrupts();
 }
 void GenerateImpulse()
 { 
-  TCNT1 = 0; //Reset cou  nter. 
+  TCNT1 = 0; //Reset counter. 
+
+  PORTB &= 0b11111101; //Set output to LOW
+  while(TCNT1 <= 10); //Wait 3uS. Noice canceler delay 4 clocks. It is 2uS. 
   
   PORTB |= 0b00000010; //Start signal
 
-  while( TCNT1 <= 20); //Wait until impulse is generated. 20 cycles are equal to 10uS.
+  while(TCNT1 <= 34); //Wait until impulse is generated. 20 cycles are equal to 10uS.
       
   PORTB &= 0b11111101; //Finish signal
 
 }
-ISR(TIMER1_CAPT_vect) //input capture 
+float CaptureInput()
 {
-  if(1) //if rising edge is set
-  {
+  if(t_start != ICR1)
+    t_stop = ICR1;
+
+  
+}
+ISR(TIMER1_CAPT_vect) //input capture interrupt.
+{
+
     t_start = ICR1;
 
-    //set to falling edge
+  
+  /*
+  if((TCCR1B &= 0b01000000)  == 0b01000000) //if rising edge is set
+  {
+    t_start = ICR1;
+    Serial.println("Hello");
+    TCCR1B &= 0b10111111; //set to falling edge
     
   }
   else
   {
      t_stop = ICR1;
-     //set to rising edge
+     TCCR1B |= (1 << ICES1); //set to rising edge
 
-     Serial.print(((t_stop - t_start)*0.5)/0.58);
+     temp = (float)((t_stop - t_start)*0.5)/58;
     
   }
     
-  
-    //Serial.print("Hello!");
+ */
 }
 int main(void)
 {
-  DDRB |= 0b00000010;
+  DDRB |= 0b00000010; //Set pin to output
   Serial.begin(9600);
   SetupTimer1();  
 
-  while(1)
-  {
-    GenerateImpulse();
-    
-  }
+  GenerateImpulse();
+
+  while(t_start != )
+  
 
   return 0;
 }
