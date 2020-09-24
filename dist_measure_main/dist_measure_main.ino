f52ef68ee24bbf988656b9110e77aa1037030bc9
Jakub Wysocki <jakubwysocki@protonmail.com>
2020-09-24 11:30:31 +0200

    Fixed LCD
    Fixed some LCD bugs

--- dist_measure_main/dist_measure_main.ino
+++ dist_measure_main/dist_measure_main.ino
@@ -4,30 +4,21 @@
 #include "dist_measure.h"
 #include "lcd.h"
 
-volatile float t_start = 0.0, t_stop = 0.0, temp = 0.0;
-int temp_int = 0;
+volatile float t_start = 0.0, t_stop = 0.0, distance = 0.0;
 
 int main(void)
 {
   DDRB &= 0b11111110;//echo - recive signal, PB0
   DDRB |= 0b00100010; //trigger send signal, PB1
-  Serial.begin(9600);
   Init(); //initialize LCD
-  char tekst[] = "Odleglosc: ";
-  
   setTimer1();
   
-  while(true) //dlaczego wstawienie tu funckji delay() powoduje zapętlenie całego main?
+  while(1)
   {
     genImpulse();
     _delay_ms(50);
-    sendText(tekst);
-    temp_int = (int)temp;
-    sendChar((unsigned char)temp_int);
-    Serial.println(temp);
-    _delay_ms(300);
-    Init();
-    _delay_ms(10);
+    display_lcd((int)distance);
+    _delay_ms(1000);
   }
   
 }
@@ -44,15 +35,12 @@ ISR(TIMER1_CAPT_vect) //input capture interrupt
   {
     t_stop = ICR1;
     TCCR1B |= (1 << ICES1);
-    temp = ((t_stop - t_start)*0.5)/58;
+    distance = ((t_stop - t_start)*0.5)/58;
     TIFR1 |= 0b00100000; //clear Input Capture Flag
   }
   
 }
 ISR(TIMER1_COMPA_vect) //interrupt when distance is too large. 
 {
-  sendChar('<');
-  sendChar('2');
-  sendChar('0');
-  sendChar('0');
+ //nothing
 }
--- dist_measure_main/lcd.h
+++ dist_measure_main/lcd.h
@@ -1,49 +1,33 @@
-#include <util/delay.h>
+#include <stdio.h>
 
-//RS PD2
-//E PD3
-//D4-D7 PD4-PD7
-
-void sendInstruction(unsigned char data)
-{
-  DDRD |= 0b11111100; //leave PD0 and PD1 as it was, set other pins to output
-  PORTD &= 0b11111011; //set RS to 0, because this is instruction
-  PORTD |= 0b00001000; //set E to 1
-
-  PORTD = ((PORTD & 0b11110000) |  (data >> 4));
-  PORTD &= 0b11110111; //set E to 0
-  _delay_us(300);
-  PORTD |= 0b00001000; //set E to 1
-
-  PORTD = ((PORTD & 0b11110000) |  (data & 0b00001111));
-  PORTD &= 0b11110111; //set E to 0
-  _delay_us(300);
-  PORTD |= 0b00001000; //set E to 1
-
-}
 void sendChar(unsigned char data)
 {
-  DDRD |= 0b11111100;
-  PORTD |= 0b00001100; //set E = 1 and RS = 1
-
-  PORTD = ((PORTD & 0b11110000) |  (data >> 4));
-  PORTD &= 0b11110111; //set E to 0
-  _delay_us(300);
-  PORTD |= 0b00001000; //set E to 1
-
-  PORTD = ((PORTD & 0b11110000) |  (data & 0b00001111));
-  PORTD &= 0b11110111; //set E to 0
-  _delay_us(300);
-  PORTD |= 0b00001000; //set E to 1
-
+  DDRD = 0b00111111;
+  PORTD |= 0b00110000; //RS = 1 i E = 1
+  PORTD = ((PORTD & 0b11110000) | ((data>>4) & 0b00001111)); 
+  PORTD &= 0b11101111; //E = 0
+  _delay_loop_2(5000);
+  PORTD |= 0b00010000; //E = 1
+  PORTD = ((PORTD & 0b11110000) | (data & 0b00001111)); 
+  PORTD &= 0b11101111; //E = 0
+  _delay_loop_2(5000);
+  PORTD |= 0b00010000; //E = 1
 }
-void sendText(char* str)
+void sendInstruction(unsigned char data)
 {
-  for(int i = 0; *(str+i) != 0; i++)
-    sendChar(*(str+i));
-
+  DDRD = 0b00111111;
+  PORTD &= 0b11011111; //RS = 0
+  PORTD |= 0b00010000; //E = 1
+  PORTD = ((PORTD & 0b11110000) | ((data>>4) & 0b00001111)); 
+  PORTD &= 0b11101111; //E = 0
+  _delay_loop_2(25000);
+  PORTD |= 0b00010000; //E = 1
+  PORTD = ((PORTD & 0b11110000) | (data & 0b00001111)); 
+  PORTD &= 0b11101111; //E = 0
+  _delay_loop_2(25000);
+  PORTD |= 0b00010000; //E = 1
 }
-void Init()
+void Init(void)
 {
   sendInstruction(0x33);
   sendInstruction(0x32);
@@ -52,5 +36,23 @@ void Init()
   sendInstruction(0x01);
   sendInstruction(0x06);
   sendInstruction(0x0F);
+}
+void sendText(char* str)
+{
+  for(int i = 0; *(str+i) != 0; i++)
+    sendChar(*(str+i));
+}
+void display_lcd(int distance)
+{ 
+  sendInstruction(0x01);
+  char tekst[] = "Odleglosc: ";
+  sendText(tekst);
+  char dist_char[4];
+  sprintf(dist_char, "%d", distance);
+  
+  sendInstruction(0xC0); //Druga linia
+  _delay_ms(20);
+  sendText(dist_char);
+  _delay_ms(20);
 
 }
